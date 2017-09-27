29797d3bb94f67cf1cdabfd9210ae317e3070ad9
ankith <ankibhat0574@gmail.com>
2017-09-27 14:40:20 +0530

    form name

--- f1.php
+++ f1.php
@@ -1,6 +1,6 @@
 
 <html>
-<form action="index.php" method="post">
+<form name="f1" action="index.php" method="post">
 ID: <input type="number" name="id1"></input>
 Name: <input type="text" name="name1"></input>
 <input type="submit" name="submit" value="Submit"></input>
@@ -8,5 +8,4 @@ Name: <input type="text" name="name1"></input>
 </form>
 <form name="f2" action="select.php" method="post">
 <input type="submit" name="submit" value="Log data"></input>
-
 </html>
